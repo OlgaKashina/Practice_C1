7dc2a68081c8f21aa0df719a5ddc4e2f0d7084b3
OlgaKashina <81535234+OlgaKashina@users.noreply.github.com>
2021-04-06 00:00:03 +0300

    Add files via upload

---
+++ object_cat.py
@@ -0,0 +1,12 @@
+from class_Cat import Cat
+
+cat_1 = Cat("Барон", "мальчик", 2)
+cat_2 = Cat("Сэм", "мальчик", 2)
+
+print("cat_1.name =", cat_1.name)
+print("cat_1.gender =", cat_1.gender)
+print("cat_1.age =", cat_1.age)
+
+print("cat_2.name =", cat_2.name)
+print("cat_2.gender =", cat_2.gender)
+print("cat_2.age =", cat_2.age)
